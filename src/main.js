93cdd2cc47325c6c0249cb82a8e7cac891a24e7c
Felipe Gustavo Braiani Santos <felipe.braianitech@gmail.com>
2019-08-23 18:23:55 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
+import vuetify from './plugins/vuetify';
+import VueLoading from 'vuejs-loading-plugin'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+Vue.use(VueLoading, {
+  dark: false,
+  text: 'Carregando', // default 'Loading'
+});
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data: () => {
+            return {
+                estudante: '',
+                valid: ''
+            }
+        },
+        methods: {
+            validate: function (e) {
+                this.$loading(true);
+                setTimeout(() => {
+                    this.valid = "Felipe Braiani";
+                    this.estudante = '';
+                    this.$loading(false);
+                }, 4000);
+                e.preventDefault();
+            }
+        }
+    };
 </script>
